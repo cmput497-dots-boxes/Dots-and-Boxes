cb22424c5140b81d1a68f0c8252eda272129369f
301git <boweiww@gmail.com>
2019-04-03 10:38:38 -0600

    fix bug

---
+++ github/Dots-and-Boxes-master/main.py
@@ -0,0 +1,10 @@
+import GUI
+import tkinter
+
+if __name__ == '__main__':
+    window = tkinter.Tk()
+    gui_chess_board = GUI.Chess_Board_Frame(window)
+    gui_chess_board.create_widgets(7,10)
+
+    gui_chess_board.pack()
+    window.mainloop()
